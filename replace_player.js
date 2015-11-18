893a8819a456b1af9c072955af7c14b2d7a1c5be
derrod <xlnedder@gmail.com>
2015-11-18 18:28:10 +0100

    Fixed Tabs

--- replace_player.js
+++ replace_player.js
@@ -1,16 +1,16 @@
 function html5replace() {
-	SiteOptions.video_experiments = '';
+    SiteOptions.video_experiments = '';
     var replaceplayer = function() {
-		setTimeout(function() {
-			// Don't run if we're not on a channel page
-			if (!window.Ember || !window.App || App.__container__.lookup("controller:application").get("currentRouteName") !== "channel.index") {
-				return;
-			}
-			$(".js-player").html($("<iframe>").attr("src", "http://player.twitch.tv/?html5&fullscreen=enabled&branding=false&showInfo=false&channel="+window.location.pathname.substr(1)).attr("width", "100%").attr("height", "100%").css("border", 0).attr("scrolling", "no").attr("frameborder", 0).attr("allowfullscreen", ""));
-		}, 5000);
+        setTimeout(function() {
+            // Don't run if we're not on a channel page
+            if (!window.Ember || !window.App || App.__container__.lookup("controller:application").get("currentRouteName") !== "channel.index") {
+                return;
+            }
+            $(".js-player").html($("<iframe>").attr("src", "http://player.twitch.tv/?html5&fullscreen=enabled&branding=false&showInfo=false&channel="+window.location.pathname.substr(1)).attr("width", "100%").attr("height", "100%").css("border", 0).attr("scrolling", "no").attr("frameborder", 0).attr("allowfullscreen", ""));
+        }, 5000);
     };
-	
-	replaceplayer();
+    
+    replaceplayer();
 }
 
 var script = document.createElement('script');
